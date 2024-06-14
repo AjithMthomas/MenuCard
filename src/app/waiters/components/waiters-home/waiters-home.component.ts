820d06e87dac17bc6828d0c2bef9e7b6de45fc6b
Nihadh5004 <nihadhfizz6@gmail.com>
2024-06-14 22:31:43 +0530

    captain products list added

--- src/app/waiters/components/waiters-home/waiters-home.component.ts
+++ src/app/waiters/components/waiters-home/waiters-home.component.ts
@@ -4,13 +4,36 @@ import { Component, OnInit } from '@angular/core';
   standalone:true,
   selector: 'app-waiters-home',
   templateUrl: './waiters-home.component.html',
-  styleUrls: ['./waiters-home.component.css']
+  styleUrls: ['./waiters-home.component.css'],
+  imports : []
 })
 export class WaitersHomeComponent implements OnInit {
+  products: { name: string, price: number }[] = []; // Array to hold products
 
   constructor() { }
 
   ngOnInit() {
+    // Fetch products when component initializes
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+
+    this.products = [
+      { name: 'Apple Juice', price: 100 },
+      { name: 'Orange Juice', price: 90 },
+      { name: 'Banana Smoothie', price: 120 },
+      { name: 'Grape Juice', price: 110 },
+      { name: 'Pineapple Juice', price: 95 },
+      { name: 'Strawberry Smoothie', price: 130 },
+      { name: 'Mango Lassi', price: 80 },
+      { name: 'Watermelon Juice', price: 85 },
+      { name: 'Peach Smoothie', price: 115 },
+      { name: 'Blueberry Juice', price: 105 },
+      { name: 'Lemonade', price: 75 },
+      { name: 'Kiwi Smoothie', price: 120 },
+      { name: 'Mixed Berry Smoothie', price: 125 }
+        ];
   }
 
 }
