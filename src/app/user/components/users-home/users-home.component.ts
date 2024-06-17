ba441fbe100eb991ad37771dc118128eab255414
AjithMthomas <ajithmt46958@gmail.com>
2024-06-17 22:23:17 +0530

    completed user home

--- src/app/user/components/users-home/users-home.component.ts
+++ src/app/user/components/users-home/users-home.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class UsersHomeComponent implements OnInit {
 
 categories =["Snacks","popular","Breads","Drinks","Burgers","Pastries","Coffee"]
-
+  
+  forYou =[1,2,2,3]
   ngOnInit() {
   }
 
