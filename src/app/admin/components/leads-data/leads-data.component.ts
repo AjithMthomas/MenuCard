2b0a24eb304dc1defb0cbc806194db6a3109934e
Nihadh5004 <nihadhfizz6@gmail.com>
2024-07-08 23:48:55 +0530

    added validators

--- src/app/admin/components/leads-data/leads-data.component.ts
+++ src/app/admin/components/leads-data/leads-data.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AdminService } from '../../services/admin.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertBoxComponent } from '../../../shared/components/alert-box/alert-box/alert-box.component';
 
 @Component({
   selector: 'app-leads-data',
@@ -12,6 +14,7 @@ import { AdminService } from '../../services/admin.service';
 export class LeadsDataComponent {
   private fb = inject(FormBuilder)
   adminService = inject(AdminService);
+  dialog = inject(MatDialog);
 
   leadsForm : FormGroup = this.fb.group({
     name: ['', Validators.required],
@@ -27,13 +30,25 @@ export class LeadsDataComponent {
         name: this.leadsForm.get('name')?.value,
         place: this.leadsForm.get('place')?.value,
         contact: String(this.leadsForm.get('contact')?.value), // Convert to string
-        franchise: String(this.leadsForm.get('franchiseCount')?.value), // Convert to string
+        franchise: String(this.leadsForm.get('franchiseCount')?.value || '0'), // Convert to string
         price: String(this.leadsForm.get('price')?.value) // Convert to string
       };
 
       this.adminService.submitLeads(formData).subscribe({
         next :(res) => {
           console.log(res);
+          const dialogRef = this.dialog.open(AlertBoxComponent, {
+            width: '350px',
+            data: {
+              title: 'Success',
+              message: 'Leads added succesfully',
+              confirmText: 'Ok',
+              type:'success'
+      
+            }
+          });
+          this.leadsForm.reset();
+
         },
         error : (err) =>{
           console.log('error adding leads', err);
